407ec217c15fa7dc499abddcdfe3e403cd87f0be
Stephen Lynch <stephen.lynch17@icloud.com>
2022-11-23 14:24:41 +0000

    FEAT: Added basket and ability to add products to it. NEXT: Bug hunt

--- basket/views.py
+++ basket/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_basket(request):
     """ A view to render the bage contents """
 
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ A view to add items to the basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+
+    request.session['basket'] = basket
+    return redirect(redirect_url)
