8ebb9bc9cc00815230be39fe2a329715898edc56
Stephen Lynch <stephen.lynch17@icloud.com>
2022-11-23 17:31:23 +0000

    FEAT: Added qty adjust and remove from basket. NEXT: Template Tags

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_basket(request):
@@ -21,3 +24,32 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ Adjust the qty of a specified product by the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+    
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop[item_id]
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """ A view to remove a specified item from the basket """
+
+    basket = request.session.get('basket', {})
+    try:
+        basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+    
+    except Exception as e:
+        return HttpResponse(status=500)
