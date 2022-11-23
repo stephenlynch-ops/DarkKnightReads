93a991fb6e017a3db5a9e75c690d3a7aae7cef3d
Stephen Lynch <stephen.lynch17@icloud.com>
2022-11-23 11:48:11 +0000

    FIX: Fixed no image not loading

--- products/views.py
+++ products/views.py
@@ -16,6 +16,7 @@ def all_products(request):
     if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
+            query = request.GET['category']
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
