ec2f13c7bcf360e1999fe70dd76946ec55469933
Stephen Lynch <stephen.lynch17@icloud.com>
2022-11-24 12:10:43 +0000

    Added toasts and fixed some minor layout bugs. NEXT: Add checkout app to project

--- basket/views.py
+++ basket/views.py
@@ -13,14 +13,17 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ A view to add items to the basket """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(request, f'Quantity changed for {product.title} to {quantity} in your basket')
     else:
         basket[item_id] = quantity
+        messages.success(request, f'Added {product.title} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
@@ -29,13 +32,16 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """ Adjust the qty of a specified product by the specified amount """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     basket = request.session.get('basket', {})
     
     if quantity > 0:
         basket[item_id] = quantity
+        messages.success(request, f'Quantity updated for {product.title} to {quantity} in your basket')
     else:
         basket.pop[item_id]
+        messages.success(request, f'Removed {product.title} from your basket')
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -44,12 +50,15 @@ def adjust_basket(request, item_id):
 def remove_from_basket(request, item_id):
     """ A view to remove a specified item from the basket """
 
+    product = get_object_or_404(Product, pk=item_id)
     basket = request.session.get('basket', {})
     try:
         basket.pop(item_id)
+        messages.success(request, f'Removed {product.title} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
     
     except Exception as e:
+        messages.error(request, f'Error removing items: {e}')
         return HttpResponse(status=500)
