fff336bcf163dfcf29651d913470a1aa63508c5b
Stephen Lynch <stephen.lynch17@icloud.com>
2022-11-26 13:32:33 +0000

    FEAT: Started to wire in webhooks. NEXT: Finish webhooks

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -53,6 +53,18 @@ form.addEventListener('submit', function(ev) {
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.full_name.value),
+                email: $.trim(form.full_name.value),
+                address:{
+                    line1: $trim(form.street_address1.value),
+                    line2: $trim(form.street_address2.value),
+                    city: $trim(form.town_or_city.value),
+                    country: $trim(form.country.value),
+                    state: $trim(form.county.value),
+                }
+            }
         }
     }).then(function(result) {
         if (result.error) {
