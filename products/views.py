9c86955f72dc0c504fe296f17d4fc84c3f8e476f
Stephen Lynch <stephen.lynch17@icloud.com>
2022-11-28 10:11:05 +0000

    FEAT: Added superuser ability to add products. NEXT: Editing existing products

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -73,3 +74,24 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The new product has been added to the store')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, "Your new product couldn't be added at this time. Please check the form for errors")
+    else:
+        form = ProductForm()
+
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
